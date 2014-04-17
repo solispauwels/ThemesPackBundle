56985828ad8afe739baa4602ac4d9263c051282a
Jorge Solis Pauwels <jorgeluissolis@gmail.com>
2014-04-17 10:26:38 +0200

    first commit

---
+++ ThemesPackBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SolisPauwels\ThemesPackBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+/**
+ * Bundle class.
+ */
+class SolisPauwelsThemesPackBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+}
